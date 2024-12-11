7cf062c7ef45f441fc0a78ff8e0d5af0cf3e3262
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2024-12-12 01:07:28 +0500

    feat: code was optimized

--- app/src/main/java/com/akbarovdev/mywallet/features/wallet/ui/WalletScreen.kt
+++ app/src/main/java/com/akbarovdev/mywallet/features/wallet/ui/WalletScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -21,10 +20,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.akbarovdev.mywallet.core.helper.SnackBarManager
 import com.akbarovdev.mywallet.features.budget.ui.view_model.BudgetViewModel
 import com.akbarovdev.mywallet.features.common.components.HomeAppBar
